6077d2ff6838a73e57e957b29b7a31aa76a3331c
ooo3345sjh <ooo3345sjh@gmail.com>
2022-08-22 19:51:38 +0900

    2022/08/22 제1장SQL 기초 제목 수정

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -2,11 +2,11 @@
 #이름 : 서정현
 #내용 : 제1장 SQL 기초
 
-#실습1-1
+#실습1-1 데이터베이스 생성 및 제거
 create database `UserDB`;
 drop database `UserDB`;
 
-#실습1-2
+#실습1-2 테이블 생성 및 제거
 create table `User1` (
 	`uid`	varchar(10),
     `name`	varchar(10),
@@ -16,7 +16,7 @@ create table `User1` (
 
 drop table `User1`;
 
-#실습1-3
+#실습1-3 테이블에 데이터 추가
 insert into `User1`values ('A101', '김유신', '010-1234-1111', 25);
 insert into `User1` values ('A102', '김춘추', '010-1234-2222', 23);
 insert into `User1` values ('A103', '장보고', '010-1234-3333', 32);
@@ -26,7 +26,7 @@ insert into `User1` set
                     `name`='이순신',
                     `hp`='010-1234-5555';
 
-#실습1-4
+#실습1-4 테이블에 데이터 조회
 select * from `User1`;
 select * from `User1` where `uid`='A101';
 select * from `User1` where `name` = '김춘추';
@@ -34,7 +34,7 @@ select * from `User1` where `age` < 30;
 select * from `User1` where `age` >= 30;
 select `uid`, `name`, `age` from `User1`;
 
-#실습1-5
+#실습1-5 테이블에 데이터 수정
 update `User1` set `hp`='010-1234-4444' where `uid`='A104';
 update `User1` set `age`='51' where `uid`='A105';
 update `User1` set 
@@ -43,7 +43,7 @@ update `User1` set
 				where 
 					`uid`='A101';
 
-#실습1-6
+#실습1-6 테이블에 데이터 삭제
 delete from `User1` where `uid`= 'A101';
 delete from `User1` where `uid`='A102' and `age`=23;
 delete from `User1` where `age`>=30;
