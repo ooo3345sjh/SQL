0123ce56b1c627bae130b8f5967b33ffc54adefb
ooo3345sjh <ooo3345sjh@gmail.com>
2022-08-25 13:31:33 +0900

    2022/08/25 SQL워크북실습예제 연습sql 추가

--- 연습.sql
+++ 연습.sql
@@ -14,13 +14,13 @@ create table `Member`(
 create table `Department`(
 	`depNO` int primary key,
     `name` varchar(10) not null,
-    `tel` int not null
+    `tel` char(12) not null
 );
 
 ##매출 테이블
 create table `Sales` (
 	`seq` int primary key auto_increment,
-    `uid`varchar(10) not null,
+    `uid` varchar(10) not null,
     `year` year not null,
     `month` tinyint not null,
     `sale` int not null
@@ -48,6 +48,83 @@ insert into `Department` values(105, '영업5부', '051-512-1005');
 insert into `Department` values(106, '영업지원부', '051-512-1006');
 insert into `Department` values(107, '인사부', '051-512-1007');
 
+##매출 테이블에 데이터 입력
+insert into `Sales` (`uid`, `year`, `month`, `sale`)values('a101', '2018', 1, 98100);
+insert into `Sales` set
+	`uid`='a102',
+    `year`=2018,
+    `month`=1,
+    `sale`=136000;
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a103', 2018, 1, 80100);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a104', 2018, 1, 78000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a105', 2018, 1, 93000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a101', 2018, 2, 23500);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a102', 2018, 2, 126000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a103', 2018, 2, 18500);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a105', 2018, 2, 19000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a106', 2018, 2, 53000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a101', 2019, 1, 24000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a102', 2019, 1, 109000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a103', 2019, 1, 101000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a104', 2019, 1, 53500);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a107', 2019, 1, 24000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a102', 2019, 2, 160000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a103', 2019, 2, 101000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a104', 2019, 2, 43000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a105', 2019, 2, 24000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a106', 2019, 2, 109000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a102', 2020, 1, 201000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a104', 2020, 1, 63000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a105', 2020, 1, 74000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a106', 2020, 1, 122000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a107', 2020, 1, 111000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a102', 2020, 2, 120000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a103', 2020, 2, 93000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a104', 2020, 2, 84000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a105', 2020, 2, 180000);
+insert into `Sales` (`uid`, `year`, `month`, `sale`) values('a108', 2020, 2, 76000);
+ 
+    
+select * from `sales`;
 select * from `department`;
+select * from `member`;
+
+##조건에 일치하는 레코드 조회
+select * from `member` where `name` = '김유신';
+select * from `member` where `pos`='차장' and `dep`=101;
+select * from `member` where `pos`='차장' or `dep`=101;
+select * from `member` where `name` != '김춘추';
+select * from `member` where `name` <> '김춘추';
+select * from `member` where `pos`='사원' or `pos`='대리';
+select * from `member` where `pos` in('사원', '대리');
+select * from `member` where `dep` in(101, 102, 103);
+select * from `member` where `name` like '%신';
+select * from `member` where `name`like '김%';
+select * from `member` where `name` like '김__';
+select * from `member` where `name` like '_성_';
+select * from `member` where `name` like '정_';
+select * from `Sales` where `sale` > 50000;
+select * from `Sales` where `sale` >= 50000 and `sale` < 100000 and `month` = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
